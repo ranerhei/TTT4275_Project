295c23069b0e24d4db0c1e03ab8425f330664e29
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2024-04-05 19:15:51 +0200

    Oppsie

---
+++ task1_description.py
@@ -0,0 +1,12 @@
+"""
+Inputs:
+-Sepal width
+-Sepal height
+-Petal width
+-Petal height
+
+Tasks:
+design and evaluation of a linear classifier
+analyze the relative importance of each of the four feature with respect to linear separability
+
+"""
