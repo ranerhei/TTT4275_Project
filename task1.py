9f20588841bb256e127ab0851349a6425645dcbd
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2024-04-08 16:37:18 +0200

    made complete data set, defined D,C and W

--- task1.py
+++ task1.py
@@ -9,6 +9,7 @@ test sets,      M=20
 
 import csv
 import matplotlib as plt
+import numpy as np
 
 #data importer
 def load_csv(filename):
@@ -19,14 +20,17 @@ def load_csv(filename):
         csv_reader = csv.reader(file)
         #iterate through data and append
         for row in csv_reader:
-            data.append([float(val) for val in row])
+            #add a weight at the end of each sample
+            data.append([float(val) for val in row]+ [1.0])
     return data
 
-
+#load data
+#data has [sepal_length, sepal_width. petal_length, petal_width]
 class_1 = load_csv('Iris_TTT4275/class_1')
 class_2 = load_csv('Iris_TTT4275/class_2')
 class_3 = load_csv('Iris_TTT4275/class_3')
 
+#make training and testing set
 class_1_training = class_1[:30]
 class_1_testing = class_1[30:]
 
@@ -35,3 +39,17 @@ class_2_testing = class_2[30:]
 
 class_3_training = class_3[:30]
 class_3_testing = class_3[30:]
+
+#make a total training data set
+training_data = [class_1_training, class_2_training, class_3_training]
+
+print(class_1[0])
+
+D = len(class_1[0])-1
+C = len(training_data)
+
+W = np.ones((C, D+1))
+print(W)
+
+g = training_data[0][0] * W
+print(g)
