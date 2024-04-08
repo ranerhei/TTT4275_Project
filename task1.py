0cc293d74ec2f0bf6c739ff060e9692c98fc3025
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2024-04-08 15:39:16 +0200

    Feat: made load_csv() function

--- task1.py
+++ task1.py
@@ -6,3 +6,22 @@ training sets,  N=30
 test sets,      M=20
 
 """
+
+import csv
+import matplotlib as plt
+
+#data importer
+def load_csv(filename):
+    #make data array
+    data = []
+    #open file
+    with open(filename, 'r') as file:
+        csv_reader = csv.reader(file)
+        #iterate through data and append
+        for row in csv_reader:
+            data.append([float(val) for val in row])
+    return data
+
+class_1 = load_csv('Iris_TTT4275/class_2')
+class_2 = load_csv('Iris_TTT4275/class_2')
+class_3 = load_csv('Iris_TTT4275/class_2')
