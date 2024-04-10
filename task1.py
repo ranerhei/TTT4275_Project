a8e7fa91f36019286a16f14889d30d8dd75feddb
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2024-04-10 18:38:55 +0200

    Single iteration of training

--- task1.py
+++ task1.py
@@ -24,12 +24,19 @@ def load_csv(filename):
             data.append([float(val) for val in row]+ [1.0])
     return data
 
-#activator function
+#activator function, equation 20
 def sigmoid(input_vector):
     return 1 / (1 + np.exp(-input_vector))
 
-def MSE_function(calculated_vector, target_vector):
-    return 0.5 * np.dot(np.transpose(calculated_vector - target_vector),calculated_vector - target_vector)
+#function to calculate Mean Square Error, equation 19
+def MSE_function(g, t):
+    return 0.5 * np.dot(np.transpose(g - t),g - t)
+
+#function to calculate gradient of MSE, equation 22
+def grad_MSE_function(g,t,x):
+    calculated_vector = np.multiply( np.multiply(g - t, g), 1-g)
+    calculated_vector = np.reshape(calculated_vector, (C,1))
+    return np.outer( calculated_vector, x)
 
 #load data
 #data has [sepal_length, sepal_width. petal_length, petal_width]
@@ -50,7 +57,6 @@ class_3_testing = class_3[30:]
 #make a total training data set
 training_data = [class_1_training, class_2_training, class_3_training]
 
-print(class_1[0])
 
 D = len(class_1[0])-1
 C = len(training_data)
@@ -58,6 +64,7 @@ C = len(training_data)
 W = np.ones((C, D+1))
 # store total Mean Square Error
 MSE = 0
+gradMSE = 0
 
 # set of target vectors
 target_vectors = {
@@ -67,15 +74,23 @@ target_vectors = {
 }
 
 #step size, alpha
-alha = 1
-
+alpha = 1
+print(W)
 #iterate over every class
 for i in range(len(training_data)):
     #define the target vector, aka correct class
-    target_vector = target_vectors.get(i, [0,0,0])
+    t = target_vectors.get(i, [0,0,0])
     #iterate over every data point
     for j in range(len(training_data[i])):
         #perform matrix multiplication
-        print(np.dot(W,training_data[i][j]))
-        print(sigmoid(np.dot(W,training_data[i][j])))
-        MSE += MSE_function(sigmoid(np.dot(W,training_data[i][j])), target_vector)
+        x = training_data[i][j]
+        g = np.dot(W,x)
+        g = sigmoid(g)
+        MSE += MSE_function(g, t)
+        #remember to remove the last weight of x!
+        gradMSE += grad_MSE_function(g,t,x)
+
+W = W-alpha*gradMSE
+print("AAAA")
+print(W)
+print(gradMSE)
